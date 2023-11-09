2bca2a54bcb64d907ca9950d15e77488070ebefc
Grace Melchers <gracem014@gmail.com>
2023-11-08 16:07:31 -0800

    fixed function, added dplyr:: to group_by

--- R/count_missing_values.R
+++ R/count_missing_values.R
@@ -45,7 +45,7 @@ count_all_missing_by_group <- function(data, group_col, .groups = "drop") {
     }
   }
 
-  data |> group_by({{ group_col }}) |>
+  data |> dplyr::group_by({{ group_col }}) |>
     dplyr::summarize(dplyr::across(everything(), ~sum(is.na(.x))),
               .groups = .groups)
 }
